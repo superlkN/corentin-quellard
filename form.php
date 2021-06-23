29a3d70487a72e0d03ab3555b425ee1c82213bdf
Corentin Quellard <corentin.quellard@gmail.com>
2021-06-23 16:16:03 +0200

    fixed sending email

--- form.php
+++ form.php
@@ -1,26 +1,35 @@
 <?php
-function sendMail() {
-    $destinataire = 'corentin.quellard@gmail.com';
-    $expediteur = $_POST['email'];
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $objet = $_POST['subject'];
-        
-    $headers  = 'MIME-Version: 1.0' . "\n";
-    $headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n";
-    $headers .= 'To: '.$destinataire."\n";
-    $headers .= 'From: "Nom_de_destinataire"<'.$expediteur.'>'."\n";
-        
-    $message = 	'<div style="width: 100%; text-align: center; font-weight: bold"> Bonjour '. $fname . ' ' . $lname .'!<br>
-                '.$_POST['subject'].'</div>';
+    function sendMail() {
+        $destinataire = 'corentin.quellard@gmail.com';
+        $expediteur = $_POST['email'];
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $objet = $_POST['object'];
 
-    mail($destinataire, $objet, $message, $headers);
-}
+        $headers = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+            
+        $message = 	'<div style="width: 100%; text-align: center; font-weight: bold"> 
+        Prénom Nom : '.$fname.' '.$lname.'<br>
+        E-mail : '. $expediteur.'<br>
+        Message :<br>'
+        .$_POST['subject'].'</div>';        
+        
+        if (mail($destinataire, $objet, $message, $headers))
+        {
+            header('refresh:0.3;url=http://corentin-quellard.com/');
+            echo '<script languag="javascript" >alert("Votre message a bien été envoyé, vous allez être redirigez..");</script>';
+        } 
+        else 
+        {
+            header('refresh:0.5;url=http://corentin-quellard.com/');
+            echo '<script languag="javascript">alert("Votre message n\'a pas pu être envoyé, vous allez être redirigez..");</script>';
+        }
+    }
 
-if ($_GET['action'] == 'mail')
-{
-    sendMail();
-    header('Location: http://corentin-quellard.com/');
-}
-    
-?>
+    if ($_GET['action'] == 'mail')
+    {
+        sendMail();
+    }
+        
+    ?>
