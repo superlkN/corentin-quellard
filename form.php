5bea920d285ae176b4461aaf4cf783cde0697fbe
Corentin Quellard <corentin.quellard@gmail.com>
2021-06-24 15:13:22 +0200

    fixed sending email when empty field

--- form.php
+++ form.php
@@ -4,7 +4,7 @@
         $expediteur = $_POST['email'];
         $fname = $_POST['fname'];
         $lname = $_POST['lname'];
-        $objet = $_POST['object'];
+        $object = $_POST['object'];
 
         $headers = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
@@ -13,17 +13,25 @@
         Prénom Nom : '.$fname.' '.$lname.'<br>
         E-mail : '. $expediteur.'<br>
         Message :<br>'
-        .$_POST['subject'].'</div>';        
+        .$_POST['subject'].'</div>';
         
-        if (mail($destinataire, $objet, $message, $headers))
+        if (!empty($expediteur) && !empty($fname) && !empty($lname) && !empty($object) && !empty($message))
         {
-            header('refresh:0.3;url=http://corentin-quellard.com/');
-            echo '<script languag="javascript" >alert("Votre message a bien été envoyé, vous allez être redirigez..");</script>';
-        } 
-        else 
+            if (mail($destinataire, $object, $message, $headers))
+            {
+                header('refresh:0;url=http://corentin-quellard.com/');
+                echo '<script languag="javascript" >alert("Votre message a bien été envoyé, vous allez être redirigez..");</script>';
+            } 
+            else 
+            {
+                header('refresh:0;url=http://corentin-quellard.com/');
+                echo '<script languag="javascript">alert("Votre message n\'a pas pu être envoyé, vous allez être redirigez..");</script>';
+            }
+        }
+        else
         {
-            header('refresh:0.5;url=http://corentin-quellard.com/');
-            echo '<script languag="javascript">alert("Votre message n\'a pas pu être envoyé, vous allez être redirigez..");</script>';
+            header('refresh:0;url=http://corentin-quellard.com/#Contact');
+            echo '<script languag="javascript" >alert("Veuillez remplir chaque champs du formulaire avant de l\'envoyer svp..");</script>';
         }
     }
 
